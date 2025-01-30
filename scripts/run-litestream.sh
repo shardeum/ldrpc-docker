2392883fb38041f18febf50b249e00ed4621e1fa
Shane Jonas <jonas.shane@gmail.com>
2025-01-30 11:57:47 -0500

    fix: make litestream urls configurable via env variable

--- scripts/run-litestream.sh
+++ scripts/run-litestream.sh
@@ -1,4 +1,13 @@
 
+
+# only set these if they arent set in the environment
+if [ -z "$COLLECTOR_LITESTREAM_URL" ]; then
+    export COLLECTOR_LITESTREAM_URL="gcs://ldrpc-seed-data/itn4/collector"
+fi
+if [ -z "$SERVICE_VALIDATOR_LITESTREAM_URL" ]; then
+    export SERVICE_VALIDATOR_LITESTREAM_URL="gcs://ldrpc-seed-data/itn4/shardeum"
+fi
+
 # convert base64 to json
 cat /home/node/dummy.json | base64 --decode > /home/node/dummy_creds.json
 export GOOGLE_APPLICATION_CREDENTIALS="/home/node/dummy_creds.json"
@@ -6,11 +15,11 @@ export GOOGLE_APPLICATION_CREDENTIALS="/home/node/dummy_creds.json"
 # if we have a collector database already dont litestream restore it
 if [ ! -f "$COLLECTOR_DB_PATH" ]; then
     echo "Restoring collector database at $COLLECTOR_DB_PATH"
-    litestream restore -o "$COLLECTOR_DB_PATH" gcs://ldrpc-seed-data/itn4/collector
+    litestream restore -o "$COLLECTOR_DB_PATH" "$COLLECTOR_LITESTREAM_URL"
 fi
 
 # if we have a service validator database already dont litestream restore it
 if [ ! -f "$SERVICE_VALIDATOR_DB_PATH" ]; then
     echo "Restoring service validator database at $SERVICE_VALIDATOR_DB_PATH"
-    litestream restore -o "$SERVICE_VALIDATOR_DB_PATH" gcs://ldrpc-seed-data/itn4/shardeum
+    litestream restore -o "$SERVICE_VALIDATOR_DB_PATH" "$SERVICE_VALIDATOR_LITESTREAM_URL"
 fi
